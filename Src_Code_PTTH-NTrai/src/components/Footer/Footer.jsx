a640c882b6f58e061228a3167f96542b5ea087fe
Dwcks Dinh <corclan19@gmail.com>
2024-02-20 10:26:20 +0700

    Added google map

--- Src_Code_PTTH-NTrai/src/components/Footer/Footer.jsx
+++ Src_Code_PTTH-NTrai/src/components/Footer/Footer.jsx
@@ -5,10 +5,14 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <div className={styles.footerLogo}>
-            <img 
+            {/* <img 
                src={pic} 
                alt="Logo"
-            />
+            /> */}
+            <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.254729970705!2d108.16609980991045!3d16.052265939829297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142196d9a203685%3A0x4e8027fe58d65525!2zQ2FvIMSR4bqzbmcgRlBUIEPGoSBT4bufIDI!5e0!3m2!1svi!2s!4v1708398705957!5m2!1svi!2s" 
+            width="400" 
+            height="300" style={{border:0 + 'px'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             <p>
              Số 137 Nguyễn Thị Thập, Phường Hòa Minh, Quận Liên Chiểu, TP. Đà Nẵng.
             </p>
